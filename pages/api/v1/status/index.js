8632f1ac4942c865e88bbe724827574e333f4bbf
BRUNO SILVA DO CANTO <75629284+Bscanto@users.noreply.github.com>
2026-01-24 16:08:37 +0000

    finish 'api/v1/status' endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,9 +2,34 @@ import database from "infra/database.js";
 
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1+1 as sum;');
-  console.log(result.rows);
-  response.status(200).json({ chave: 'São acima da média!' });
+  const updateAt = new Date(). toISOString();
+
+  const dataBaseVersionResult = await database.query(
+    "SHOW server_version;");
+  const databaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+
+  const databaseMaxConnectonsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectonsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenndConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const databaseOpennedConnectionsValue = databaseOpenndConnectionsResult.rows[0].count;
+  
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpennedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status; 
